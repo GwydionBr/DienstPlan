b27fd772054c1865a2d51d74f8da0884b7fda298
GwydionBr <gwydie@googlemail.com>
2024-08-21 12:16:07 +0200

    init department, fixed_worker, relative_worker and dienstplan classes

---
+++ dienstplan.py
@@ -0,0 +1,4 @@
+class Dienstplan:
+    
+    def __init__(self, name):
+      self.name = name
---
+++ relative_worker.py
@@ -0,0 +1,4 @@
+class Relative_Worker:
+    def __init__(self, name: str, workhours: int, holiday_days: int):
+        self.name = name
+        
